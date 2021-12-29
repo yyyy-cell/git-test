816066e78ff02596528659b50350573ce3b989cb
yanyong <1972335255@qq.com>
2021-12-30 00:54:43 +0800

    push commit

--- src/main/java/git/test/test.java
+++ src/main/java/git/test/test.java
@@ -8,5 +8,6 @@ public class test {
         System.out.println("hello git4");
         System.out.println("hot-fix commit");
         System.out.println("master commit");
+        System.out.println("push commit");
     }
 }
